f7121a6be430f6dd65bd7ec5b7e68962815956c4
cjh1249131356 <cjh1249131356@gmail.com>
2024-11-06 20:36:28 +0530

    oplus-fwk: Add missing classes for oplus camera
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Change-Id: I625657e790b14aea606fd6b6aa3c5a8434e6d888

--- oplus-fwk/src/com/oplus/uifirst/OplusUIFirstManager.java
+++ oplus-fwk/src/com/oplus/uifirst/OplusUIFirstManager.java
@@ -1,17 +1,15 @@
-/*
- * SPDX-FileCopyrightText: 2025 The LineageOS Project
- * SPDX-License-Identifier: Apache-2.0
- */
-
 package com.oplus.uifirst;
 
-public class OplusUIFirstManager implements IOplusUIFirstManager {
-    private static OplusUIFirstManager sInstance = null;
+public class OplusUIFirstManager {
+    
+    private static OplusUIFirstManager sOplusUIFirstManager = null;
 
     public static OplusUIFirstManager getInstance() {
-        if (sInstance == null) {
-            sInstance = new OplusUIFirstManager();
+        if (sOplusUIFirstManager == null) {
+            sOplusUIFirstManager = new OplusUIFirstManager();
         }
-        return sInstance;
+        return sOplusUIFirstManager;
     }
+
+    public void setUxThreadValue(int pid, int tid, String value) {}
 }
